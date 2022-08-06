645d75cb0f6e239ce8882b13674d1d5f88b8f947
Hoàng Minh Trí <87680070+HMT2008@users.noreply.github.com>
2022-08-06 19:38:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const server = express();
 
 server.all("/", (req, res) => {
-  res.send("OK!");
+  res.send("Nice!");
 });
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Server is Ready!");
+    console.log("Discord Online 24/7 is Ready!");
   });
 }
 
